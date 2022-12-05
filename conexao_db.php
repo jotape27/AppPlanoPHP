631effe1cdaaf3edd1519c18ebfd2cbbd61202fc
jp-027 <jplferreira27@gmail.com>
2022-12-05 12:03:53 -0300

    2

--- conexao_db.php
+++ conexao_db.php
@@ -6,14 +6,14 @@ $host        = "host = abul.db.elephantsql.com;";
 $port        = "port = 5432;";
 $dbname      = "dbname = apgrsynx;";
 $dbuser      = "apgrsynx";
-$dbpassword     = "Feg9QwZGipsWx7MwTMPRsXwHIrcAROgd";
+$dbpassword  = "Feg9QwZGipsWx7MwTMPRsXwHIrcAROgd";
 
 // dados de conexao com o railway. Usar somente caso esteja usando railway
 // $host        = "host = " . getenv("PGHOST") . ";";
 // $port        = "port = " . getenv("PGPORT") . ";";
 // $dbname      = "dbname = " . getenv("PGDATABASE") . ";";
-// $dbuser 	 = getenv("PGUSER");
-// $dbpassword	 = getenv("PGPASSWORD");
+// $dbuser 	    = getenv("PGUSER");
+// $dbpassword  = getenv("PGPASSWORD");
 
 // para conectar ao mysql, substitua pgsql por mysql
 $db_con = new PDO('pgsql:' . $host . $port . $dbname, $dbuser, $dbpassword);
