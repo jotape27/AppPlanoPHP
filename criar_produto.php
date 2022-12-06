a83d7fb41f82a1b2ea433fb7148db352511afbe1
jotape27 <jplferreira27@gmail.com>
2022-12-06 16:30:04 -0300

    mais modificações

--- criar_produto.php
+++ criar_produto.php
@@ -17,66 +17,84 @@ $resposta = array();
 // verifica se o usuário conseguiu autenticar
 if (autenticar($db_con)) {
 
-	// Primeiro, verifica-se se todos os parametros foram enviados pelo cliente.
-	// A criacao de um produto precisa dos seguintes parametros:
-	// nome - nome do produto
-	// preco - preco do produto
-	// descricao - descricao do produto
-	// img - imagem do produto
-	if (isset($_POST['valor']) && isset($_POST['gasto'])) {
+    // Primeiro, verifica-se se todos os parametros foram enviados pelo cliente.
+    // A criacao de um produto precisa dos seguintes parametros:
+    // nome - nome do produto
+    // preco - preco do produto
+    // descricao - descricao do produto
+    // img - imagem do produto
+    if (isset($_POST['valor']) && isset($_POST['gasto']) && isset($_POST['tipogasto'])) {
 
-		// Aqui sao obtidos os parametros
-		$nome = $_POST['gasto'];
-		$valor = $_POST['valor'];
+        // Aqui sao obtidos os parametros
+        $nome = $_POST['gasto'];
+        $tipo = $_POST['tipogasto'];
+        $valor = $_POST['valor'];
 
-		// Para a imagem do produto, primeiramente se determina qual o tipo de imagem.
-		// Isso e feito atraves da obtencao da extensao do arquivo, localizada na parte
-		// final do nome do arquivo (ex. ".jpg")
+        // Para a imagem do produto, primeiramente se determina qual o tipo de imagem.
+        // Isso e feito atraves da obtencao da extensao do arquivo, localizada na parte
+        // final do nome do arquivo (ex. ".jpg")
 
-		/*$imageFileType = strtolower(pathinfo(basename($_FILES["img"]["name"]), PATHINFO_EXTENSION));*/
+        /*$imageFileType = strtolower(pathinfo(basename($_FILES["img"]["name"]), PATHINFO_EXTENSION));*/
 
-		// A imagem e convertida de binario para string atraves do metodo de codificacao
-		// base64
+        // A imagem e convertida de binario para string atraves do metodo de codificacao
+        // base64
 
-		/*$image_base64 = base64_encode(file_get_contents($_FILES['img']['tmp_name']));*/
+        /*$image_base64 = base64_encode(file_get_contents($_FILES['img']['tmp_name']));*/
 
-		// No futuro, clientes que pedirem pela imagem armazenada no BD devem ser 
-		// capazes de converter a string base64 para o formato original binario.
-		// Para que isso possa ser feito, contatena-se no inicio da string base64 da 
-		// imagem o mimetype do arquivo original. O mimetype e um codigo que indica o 
-		// tipo de arquivo e sua extensao.
+        // No futuro, clientes que pedirem pela imagem armazenada no BD devem ser
+        // capazes de converter a string base64 para o formato original binario.
+        // Para que isso possa ser feito, contatena-se no inicio da string base64 da
+        // imagem o mimetype do arquivo original. O mimetype e um codigo que indica o
+        // tipo de arquivo e sua extensao.
 
-		/*$img = 'data:image/' . $imageFileType . ';base64,' . $image_base64;*/
+        /*$img = 'data:image/' . $imageFileType . ';base64,' . $image_base64;*/
 
-		// A proxima linha insere um novo produto no BD.
-		// A variavel consulta indica se a insercao foi feita corretamente ou nao.
+        // A proxima linha insere um novo produto no BD.
+        // A variavel consulta indica se a insercao foi feita corretamente ou nao.
 
-		$data = date('Y-m-d');
+        $data = date('Y-m-d');
 
-		$consulta = $db_con->prepare("INSERT INTO gasto(valor, gasto, data) VALUES('$valor', '$nome', '$data')");
-		if ($consulta->execute()) {
-			// Se o produto foi inserido corretamente no servidor, o cliente 
-			// recebe a chave "sucesso" com valor 1
-			$resposta["sucesso"] = 1;
-		} else {
-			// Se o produto nao foi inserido corretamente no servidor, o cliente 
-			// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
-			// motivo da falha.
-			$resposta["sucesso"] = 0;
-			$resposta["erro"] = "Erro ao criar produto no BD: " . $consulta->error;
-		}
-	} else {
-		// Se a requisicao foi feita incorretamente, ou seja, os parametros 
-		// nao foram enviados corretamente para o servidor, o cliente 
-		// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
-		// motivo da falha.
-		$resposta["sucesso"] = 0;
-		$resposta["erro"] = "Campo requerido nao preenchido";
-	}
+        $consulta = $db_con->prepare("INSERT INTO gasto (valor, gasto, data) VALUES('$valor', '$nome', '$data') RETURNING id;");
+        if ($consulta->execute()) {
+
+            if ($consulta->rowCount() > 0) {
+                $linha = $consulta->fetch(PDO::FETCH_ASSOC);
+
+                $id_gasto = $linha['id'];
+
+                $consulta2 = $db_con->prepare("INSERT INTO usuario_tpgasto_tipo_gasto_usuario_gasto_planejamento VALUES ('$tipo',':id_user','$id_gasto');");
+
+                if ($consulta2->execute()) {
+                    // Se o produto foi inserido corretamente no servidor, o cliente
+                    // recebe a chave "sucesso" com valor 1
+                    $resposta["sucesso"] = 1;
+                } else {
+                    $resposta["sucesso"] = 0;
+                    $resposta["erro"] = "Erro ao criar produto no BD: " . $consulta->error;
+                }
+            } else {
+                $resposta["sucesso"] = 0;
+                $resposta["erro"] = "Erro ao criar produto no BD: " . $consulta->error;
+            }
+        } else {
+            // Se o produto nao foi inserido corretamente no servidor, o cliente
+            // recebe a chave "sucesso" com valor 0. A chave "erro" indica o
+            // motivo da falha.
+            $resposta["sucesso"] = 0;
+            $resposta["erro"] = "Erro ao criar produto no BD: " . $consulta->error;
+        }
+    } else {
+        // Se a requisicao foi feita incorretamente, ou seja, os parametros
+        // nao foram enviados corretamente para o servidor, o cliente
+        // recebe a chave "sucesso" com valor 0. A chave "erro" indica o
+        // motivo da falha.
+        $resposta["sucesso"] = 0;
+        $resposta["erro"] = "Campo requerido nao preenchido";
+    }
 } else {
-	// senha ou usuario nao confere
-	$resposta["sucesso"] = 0;
-	$resposta["erro"] = "usuario ou senha não confere";
+    // senha ou usuario nao confere
+    $resposta["sucesso"] = 0;
+    $resposta["erro"] = "usuario ou senha não confere";
 }
 
 // Fecha a conexao com o BD
--- registrar.php
+++ registrar.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /*
  * O código abaixo registra um novo usuário.
  * Método do tipo POST.
@@ -9,57 +9,67 @@ require_once('conexao_db.php');
 
 // array de resposta
 $resposta = array();
- 
+
 // verifica se todos os campos necessários foram enviados ao servidor
-if (isset($_POST['novo_login']) && isset($_POST['nova_senha'])) {
- 
+if (isset($_POST['novo_nome']) && isset($_POST['novo_sobrenome']) && isset($_POST['novo_login']) && isset($_POST['novo_nascimento']) && isset($_POST['novo_sexo']) && isset($_POST['novo_celular']) && isset($_POST['novo_email']) && isset($_POST['nova_senha'])) {
+
     // o método trim elimina caracteres especiais/ocultos da string
-	$novo_login = trim($_POST['novo_login']);
-	$nova_senha = trim($_POST['nova_senha']);
-	
-	// o bd não armazena diretamente a senha do usuário, mas sim 
-	// um código hash que é gerado a partir da senha.
-	$token = password_hash($nova_senha, PASSWORD_DEFAULT);
-	
-	// antes de registrar o novo usuário, verificamos se ele já
-	// não existe.
-	$consulta_usuario_existe = db_con->prepare("SELECT login FROM usuarios WHERE login='$novo_login'");
-	$consulta_usuario_existe->execute();
-	if ($consulta_usuario_existe->rowCount() > 0) { 
-		// se já existe um usuario para login
-		// indicamos que a operação não teve sucesso e o motivo
-		// no campo de erro.
-		$resposta["sucesso"] = 0;
-		$resposta["erro"] = "usuario ja cadastrado";
-	}
-	else {
-		// se o usuário ainda não existe, inserimos ele no bd.
-		$consulta = db_con->prepare("INSERT INTO usuarios(login, token) VALUES('$novo_login', '$token')");
-	 
-		if ($consulta->execute()) {
-			// se a consulta deu certo, indicamos sucesso na operação.
-			$resposta["sucesso"] = 1;
-		}
-		else {
-			// se houve erro na consulta, indicamos que não houve sucesso
-			// na operação e o motivo no campo de erro.
-			$resposta["sucesso"] = 0;
-			$resposta["erro"] = "erro BD: " . $consulta->error;
-		}
-	}
-}
-else {
-	// se não foram enviados todos os parâmetros para o servidor, 
-	// indicamos que não houve sucesso
-	// na operação e o motivo no campo de erro.
+    $novo_nome = trim($_POST['novo_nome']);
+    $novo_sobrenome = trim($_POST['novo_sobrenome']);
+    $novo_login = trim($_POST['novo_login']);
+    $novo_nascimento = trim($_POST['novo_nascimento']);
+    $novo_sexo = trim($_POST['novo_sexo']);
+    $novo_celular = trim($_POST['novo_celular']);
+    $novo_email = trim($_POST['novo_email']);
+    $nova_senha = trim($_POST['nova_senha']);
+
+    // o bd não armazena diretamente a senha do usuário, mas sim
+    // um código hash que é gerado a partir da senha.
+    $token = password_hash($nova_senha, PASSWORD_DEFAULT);
+
+    // antes de registrar o novo usuário, verificamos se ele já
+    // não existe.
+    $consulta_usuario_existe = $db_con->prepare("SELECT cpf FROM usuario WHERE cpf ='$novo_cpf'");
+    $consulta_usuario_existe->execute();
+    if ($consulta_usuario_existe->rowCount() > 0) {
+        // se já existe um usuario para login
+        // indicamos que a operação não teve sucesso e o motivo
+        // no campo de erro.
+        $resposta["sucesso"] = 0;
+        $resposta["erro"] = "usuario ja cadastrado";
+    } else {
+        // se o usuário ainda não existe, inserimos ele no bd.
+        $consulta = $db_con->prepare("INSERT INTO usuarios VALUES(DEFAULT,'$novo_nome','$novo_sobrenome','$novo_login','$novo_sexo','$novo_nascimento','$token') RETURNING id");
+
+        if ($consulta->execute()) {
+            // se a consulta deu certo, indicamos sucesso na operação.
+            $linha = $consulta->fetch(PDO::FETCH_ASSOC);
+            $id_user = $linha['id'];
+
+            $consulta2 = $db_con->prepare("INSERT INTO usuario_tpcontato VALUES ('$id_user','$novo_email','$novo_celular');");
+
+            if ($consulta2->execute()) {
+            }
+
+            $resposta["sucesso"] = 1;
+        } else {
+            // se houve erro na consulta, indicamos que não houve sucesso
+            // na operação e o motivo no campo de erro.
+            $resposta["sucesso"] = 0;
+            $resposta["erro"] = "erro BD: " . $consulta->error;
+        }
+    }
+} else {
+    // se não foram enviados todos os parâmetros para o servidor,
+    // indicamos que não houve sucesso
+    // na operação e o motivo no campo de erro.
     $resposta["sucesso"] = 0;
-	$resposta["erro"] = "faltam parametros";
+    $resposta["erro"] = "faltam parametros";
 }
 
 // A conexão com o bd sempre tem que ser fechada
 $db_con = null;
 
-// converte o array de resposta em uma string no formato JSON e 
+// converte o array de resposta em uma string no formato JSON e
 // imprime na tela.
 echo json_encode($resposta);
-?>
