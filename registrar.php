36f7656fd415abc337ea1e608ca27bdc0111a682
jotape27 <jplferreira27@gmail.com>
2022-12-06 17:56:22 -0300

    mais coisas

--- registrar.php
+++ registrar.php
@@ -1,15 +1,11 @@
 <?php
-
 /*
  * O código abaixo registra um novo usuário.
  * Método do tipo POST.
  */
-
 require_once('conexao_db.php');
-
 // array de resposta
 $resposta = array();
-
 // verifica se todos os campos necessários foram enviados ao servidor
 if (isset($_POST['novo_nome']) && isset($_POST['novo_sobrenome']) && isset($_POST['novo_login']) && isset($_POST['novo_nascimento']) && isset($_POST['novo_sexo']) && isset($_POST['novo_celular']) && isset($_POST['novo_email']) && isset($_POST['nova_senha'])) {
 
@@ -22,11 +18,9 @@ if (isset($_POST['novo_nome']) && isset($_POST['novo_sobrenome']) && isset($_POS
     $novo_celular = trim($_POST['novo_celular']);
     $novo_email = trim($_POST['novo_email']);
     $nova_senha = trim($_POST['nova_senha']);
-
     // o bd não armazena diretamente a senha do usuário, mas sim
     // um código hash que é gerado a partir da senha.
     $token = password_hash($nova_senha, PASSWORD_DEFAULT);
-
     // antes de registrar o novo usuário, verificamos se ele já
     // não existe.
     $consulta_usuario_existe = $db_con->prepare("SELECT cpf FROM usuario WHERE cpf ='$novo_cpf'");
@@ -42,16 +36,21 @@ if (isset($_POST['novo_nome']) && isset($_POST['novo_sobrenome']) && isset($_POS
         $consulta = $db_con->prepare("INSERT INTO usuarios VALUES(DEFAULT,'$novo_nome','$novo_sobrenome','$novo_login','$novo_sexo','$novo_nascimento','$token') RETURNING id");
 
         if ($consulta->execute()) {
-            // se a consulta deu certo, indicamos sucesso na operação.
+            //
             $linha = $consulta->fetch(PDO::FETCH_ASSOC);
             $id_user = $linha['id'];
-
+            //
             $consulta2 = $db_con->prepare("INSERT INTO usuario_tpcontato VALUES ('$id_user','$novo_email','$novo_celular');");
-
+            //
             if ($consulta2->execute()) {
+                // se a consulta deu certo, indicamos sucesso na operação.
+                $resposta["sucesso"] = 1;
+            } else {
+                // se houve erro na consulta, indicamos que não houve sucesso
+                // na operação e o motivo no campo de erro.
+                $resposta["sucesso"] = 0;
+                $resposta["erro"] = "erro BD: " . $consulta2->error;
             }
-
-            $resposta["sucesso"] = 1;
         } else {
             // se houve erro na consulta, indicamos que não houve sucesso
             // na operação e o motivo no campo de erro.
@@ -66,7 +65,6 @@ if (isset($_POST['novo_nome']) && isset($_POST['novo_sobrenome']) && isset($_POS
     $resposta["sucesso"] = 0;
     $resposta["erro"] = "faltam parametros";
 }
-
 // A conexão com o bd sempre tem que ser fechada
 $db_con = null;
 
